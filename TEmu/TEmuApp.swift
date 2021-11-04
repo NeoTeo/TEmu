79082c6d45071d45c1940b0946d82c083f3e5ce5
NeoTEo <m@sartori.dk>
2021-11-04 14:28:00 +0100

    Initial Commit

---
+++ TEmu/TEmuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEmuApp.swift
+//  TEmu
+//
+//  Created by teo on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TEmuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
